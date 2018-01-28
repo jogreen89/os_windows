1aad342e8a6e6a6f54a683187c4ebca1e9d0875a
jogreen89 <jonagreen89@gmail.com>
2018-01-28 12:37:12 -0500

    revision (initial commit)

--- shell/main.cpp
+++ shell/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -9,7 +10,30 @@ int main(int argc, char **argv)
 	while (status)
 	{
 		printf("osh> ");
-		// gets
+		gets_s(line);
+		parse(line, argv);
+
+		if (strcmp(argv[0], "exit"))
+		{
+			exit(0);
+		}
+		else if (strcmp(argv[0], "!!"))
+		{
+			
+		}
 		
 	}
+}
+
+int parse(char *line, char **argv)
+{
+	while (*line != '\0')
+	{
+		// while (*line == ' ' || *line == '\t' || *line == '\n'
+	}
+}
+
+void launch_osh(char **argv)
+{
+	pid
 }
