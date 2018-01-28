a5b9eb47b90b496cd45315d2df53a54bde74495e
jogreen89 <jonagreen89@gmail.com>
2018-01-28 12:36:51 -0500

    initial commit

---
+++ shell/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	char line[1024];
+	bool status = true;
+
+	while (status)
+	{
+		printf("osh> ");
+		// gets
+		
+	}
+}
